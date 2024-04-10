de3d12c0ad38e661f2d59567f3b13ee5d32ece0f
Sri Harish <srihari252000@gmail.com>
2024-04-10 00:37:06 -0400

    Extracted Quantity and People list

--- walmartBill.py
+++ walmartBill.py
@@ -1,11 +1,15 @@
 import PyPDF2
 import re
+import logging
 
 # Global Variables
 actual_subtotal = 0.0
 actual_total = 0.0
 pdf_text = ""
 shopped_items = []
+quantity_list = []
+people_list = []
+
 
 # Global Regular expression patterns
 price_with_float_pattern = r'\$\d+\.\d{2}'
@@ -20,16 +24,15 @@ def extract_text_from_pdf(pdf_path: str):
         return text
 
 def extract_and_remove_unavailable_items(initial_pdf_text: str):
-    global pdf_text
-    pdf_text = ""
+    extracted_pdf_text = ""
     unavailable_item_lines = []
     pdf_lines = []
     for line in initial_pdf_text.splitlines():
         if line.__contains__("Unavailable"):
             unavailable_item_lines.append(line)
         else:
-            pdf_text += line + "\n"
-    return unavailable_item_lines
+            extracted_pdf_text += line + "\n"
+    return unavailable_item_lines, extracted_pdf_text
 
 def validate_price_string(price: str):
     try:
@@ -139,37 +142,106 @@ def extract_shopped_items_list(input_pdf_text: str):
             shopped_items_list.append(item_description.group()[:-1])
     return shopped_items_list
 
-# Replace 'path_to_pdf.pdf' with the path to your PDF bill receipt file
-pdf_path = 'Bill3_WithRefund.pdf'
+def verify_total_match(pdf_text):
+    actual_subtotal = extract_actual_subtotal(pdf_text)
+    expected_subtotal = calculate_expected_subtotal(pdf_text)
+    print(f'Actual Subtotal: {actual_subtotal}\nExpected Subtotal: {expected_subtotal}')
+
+    if actual_subtotal == expected_subtotal:
+        print("SubTotal matches!")
+
+    fees = extract_fees(pdf_text)
+    print(f'Fees: {fees}')
+
+    actual_total = extract_total(pdf_text)
+    print(f'Actual Total Price: {actual_total}')
+
+    expected_total = round(expected_subtotal + fees, 2)
+    print(f'Expected Total Price: {expected_total}')
+
+    if actual_total == expected_total:
+        print("Total matches!")
+        return True
+    return False
+
+def extract_quantity_list(shopped_items):
+    quantity_list = []
+    for item in shopped_items:
+        # Split the string at "Qty"
+        parts = item.split("Qty")
+        # Check if there's a part after "Qty"
+        if len(parts) > 1:
+            # Extract the last element (assuming "Qty" is at the end)
+            quantity = parts[-1].strip()
+        else:
+            quantity = "NA"  # Use "NA" if "Qty" is not found
+        # Add the quantity to the list
+        quantity_list.append(quantity)
+    return quantity_list
+
+def get_people_list_from_user():
+    people = []
+    while True:
+        name = input("Enter a person's name (or press Enter to finish): ")
+        # Check if user pressed Enter to finish
+        if not name:
+            break
+        people.append(name)
+    return people
+
+def get_person_contribution_list(person: str, shopped_items : list):
+    shop_list = []
+    print(f"Enter the split of each item for {person}: (Enter value >=0)")
+    for item in shopped_items:
+        split_value = input(f"{item}: ")
+        try:
+            split_value = float(split_value)
+        except ValueError:
+            logging.error("Error converting String to Float")
+            split_value = 0
+        if split_value<0:
+            logging.error("Split value must be positive")
+            exit
+        shop_list.append(split_value)
+    return shop_list
+
+####################################################
+
+# Assign pdf_path variable with the path to your PDF bill receipt file
+pdf_path = 'resources/billPDFs/Bill2_WithUnavailable.pdf'
 pdf_text = extract_text_from_pdf(pdf_path)
 
 if len(pdf_text)<2:
-    print("Unable to extract text, Image based PDF or incorrect PDF selected!")
+    logging.error("Unable to extract text, Image based PDF or incorrect PDF selected!")
     exit
 
-unavailable_item_lines = extract_and_remove_unavailable_items(pdf_text)
+unavailable_item_lines, pdf_text = extract_and_remove_unavailable_items(pdf_text)
+
+verify_total_match(pdf_text)
+
 
 shopped_items = extract_shopped_items_list(pdf_text)
 print(shopped_items)
 
+quantity_list = extract_quantity_list(shopped_items)
+print(quantity_list) 
 
-actual_subtotal = extract_actual_subtotal(pdf_text)
+people_list = get_people_list_from_user()
+print(people_list)
 
-expected_subtotal = calculate_expected_subtotal(pdf_text)
+if len(people_list)<1:
+    logging.error("Atleast one person must be specified, Try again!")
+    exit
+elif len(people_list)==1:
+    print("Why are you even here? There's no one else to split the bill with.")
+    exit
 
-print(f'Actual Subtotal: {actual_subtotal}\nExpected Subtotal: {expected_subtotal}')
+item_split_dict = {}
+for person in people_list:
+    item_split_dict[person] = get_person_contribution_list(person, shopped_items)
+print(item_split_dict)
 
-if actual_subtotal == expected_subtotal:
-    print("SubTotal matches!")
+# TODO: Create a method to calculate the value of each person
 
-fees = extract_fees(pdf_text)
-print(f'Fees: {fees}')
+# TODO: Mark the person who paid th bill, calculate how much each person owes the bill owner
 
-actual_total = extract_total(pdf_text)
-print(f'Actual Total Price: {actual_total}')
-
-expected_total = round(expected_subtotal + fees, 2)
-print(f'Expected Total Price: {expected_total}')
-
-if actual_total == expected_total:
-    print("Total matches!")
